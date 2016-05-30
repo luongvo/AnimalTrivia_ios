6adc59749999f980cf494cc37ebb373999460b9e
Luong Vo <luong.vo@eastagile.com>
2016-05-30 17:35:35 +0700

    Update

--- AnimalTrivia/ViewController.swift
+++ AnimalTrivia/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var aButton: UIButton!
+    @IBOutlet weak var bButton: UIButton!
+    @IBOutlet weak var cButton: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
