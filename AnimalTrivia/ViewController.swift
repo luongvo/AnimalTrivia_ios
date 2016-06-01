2b90c856f617f482acc62d98727c510698d25acd
Luong Vo <luong.vo@eastagile.com>
2016-06-01 17:20:21 +0700

    Update

---
+++ AnimalTrivia/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  AnimalTrivia
+//
+//  Created by East Agile on 6/1/16.
+//  Copyright © 2016 East Agile. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet weak var textField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        textField.delegate = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+
+}
